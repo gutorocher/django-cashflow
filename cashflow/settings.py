47baf05ea3bb16d6379f22a03918305917a33055
Edgar gabaldi <edgabaldi@gmail.com>
2013-01-09 15:42:59 -0200

    settings file changes for project path

--- cashflow/settings.py
+++ cashflow/settings.py
@@ -1,4 +1,7 @@
 # Django settings for cashflow project.
+import os
+
+BASE_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), os.path.pardir))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -109,6 +112,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'templates'),
 )
 
 INSTALLED_APPS = (
