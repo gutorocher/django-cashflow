c9313d9ab0288e31c78a4522192ac1fdb0dcd5b5
Edgar gabaldi <edgabaldi@gmail.com>
2013-01-10 13:39:44 -0200

    changing standard generic views to restricted generic views

--- money/views.py
+++ money/views.py
@@ -1,33 +1,33 @@
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, ModelFormMixin, ProcessFormView, DeleteView
 from django.core.urlresolvers import reverse_lazy
-from money.generic import ModelFormWithListView
+from money import generic
 from money.models import Entry, Bank, Account, Person
 from money import forms
 
-class EntryList(ListView):
+class EntryList(generic.RestrictedListView):
     model=Entry
 
-class EntryCreate(CreateView):
+class EntryCreate(generic.RestrictedCreateView):
     model=Entry
     form_class=forms.EntryForm
     success_url = reverse_lazy('entry_list')
 
-class BankList(ModelFormWithListView):
+class BankList(generic.ModelFormWithListView):
     model=Bank
     form_class=forms.BankForm
     success_url = reverse_lazy('bank_list')
 
-class BankDelete(DeleteView):
+class BankDelete(generic.RestrictedDeleteView):
     model=Bank
     success_url = reverse_lazy('bank_list')
 
-class AccountList(ModelFormWithListView):
+class AccountList(generic.ModelFormWithListView):
     model=Account
     form_class=forms.AccountForm
     success_url = reverse_lazy('account_list')
 
-class PersonList(ModelFormWithListView):
+class PersonList(generic.ModelFormWithListView):
     model=Person
     form_class=forms.PersonForm
     success_url = reverse_lazy('person_list')
