d21edeef5440ba5ec7de726d8612fc7836c714fb
Edgar gabaldi <edgabaldi@gmail.com>
2013-01-10 13:36:49 -0200

    added extended restricted generic views that checks permissions

--- money/generic.py
+++ money/generic.py
@@ -1,7 +1,49 @@
-from django.views.generic.list import ListView
-from django.views.generic.edit import ModelFormMixin, ProcessFormView
+from django.views import generic
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required, permission_required
 
-class ModelFormWithListView(ListView, ModelFormMixin, ProcessFormView):
+class RestrictedListView(generic.ListView):
+    ''' Generic list view that checks permissions '''
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        @permission_required('%s.change_%s' % (self.model._meta.app_label, self.model._meta.module_name))
+        def wrapper(request, *args, **kwargs):
+            return super(RestrictedListView, self).dispatch(request, *args, **kwargs)
+        return wrapper(request, *args, **kwargs)
+
+class RestrictedUpdateView(generic.UpdateView):
+    ''' Generic update view that checks permissions '''
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        @permission_required('%s.change_%s' % (self.model._meta.app_label, self.model._meta.module_name))
+        def wrapper(request, *args, **kwargs):
+            return super(RestrictedUpdateView, self).dispatch(request, *args, **kwargs)
+        return wrapper(request, *args, **kwargs)
+
+class RestrictedCreateView(generic.CreateView):
+    ''' Generic create view that checks permissions '''
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        @permission_required('%s.add_%s' % (self.model._meta.app_label, self.model._meta.module_name))
+        def wrapper(request, *args, **kwargs):
+            return super(RestrictedCreateView, self).dispatch(request, *args, **kwargs)
+        return wrapper(request, *args, **kwargs)
+
+class RestrictedDeleteView(generic.DeleteView):
+    ''' Generic delete view that checks permissions '''
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        @permission_required('%s.delete_%s' % (self.model._meta.app_label, self.model._meta.module_name))
+        def wrapper(request, *args, **kwargs):
+            return super(RestrictedDeleteView, self).dispatch(request, *args, **kwargs)
+        return wrapper(request, *args, **kwargs)
+
+class LoginRequired(generic.View):
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(LoginRequired, self).dispatch(request, *args, **kwargs)
+
+class ModelFormWithListView(RestrictedListView, generic.edit.ModelFormMixin, generic.edit.ProcessFormView):
 
 	def post(self, request, *args, **kwargs):
 		try:
