e48998f90534dd14517a73475159db0a4835e8d1
Edgar gabaldi <edgabaldi@gmail.com>
2013-01-10 11:41:01 -0200

    added person edit feature

--- money/urls.py
+++ money/urls.py
@@ -7,4 +7,6 @@ urlpatterns = patterns('',
     url(r'^banks/$', views_money.BankList.as_view(), name='bank_list'),
     url(r'^accounts/$', views_money.AccountList.as_view(), name='account_list'),
     url(r'^people/$', views_money.PersonList.as_view(), name='person_list'),
+    url(r'^people/edit/(?P<pk>\d+)$', views_money.PersonList.as_view(), name='person_edit'),
+
 )
