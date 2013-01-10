43a59fa4fab40414b482e475cdb35b409bdc459e
Edgar gabaldi <edgabaldi@gmail.com>
2013-01-10 11:08:54 -0200

    BankList view refactory

--- money/views.py
+++ money/views.py
@@ -13,34 +13,10 @@ class EntryCreate(CreateView):
 	form_class=forms.EntryForm
 	success_url = reverse_lazy('entry_list')
 
-class BankList(ListView, ModelFormMixin, ProcessFormView):
+class BankList(ModelFormWithListView):
 	model=Bank
 	form_class=forms.BankForm
 	success_url = reverse_lazy('bank_list')
-
-	def post(self, request, *args, **kwargs):
-		try:
-			self.object = self.get_object()
-		except:
-			self.object = None
-		return super(BankList, self).post(request, *args, **kwargs)
-
-	def get_context_data(self, **kwargs):
-
-		self.object_list = self.get_queryset()
-
-		try:
-			self.object = self.get_object()
-		except:
-			self.object = None
-
-		kwargs.update({
-			'object_list' : self.object_list,
-			'form' : self.get_form(self.get_form_class()),
-			'object' : self.object
-		})
-
-		return super(BankList, self).get_context_data(**kwargs)
 
 class AccountList(ModelFormWithListView):
 	model=Account
