b50031c5b0e0379299b50b72e3db206bf397b6ac
Edgar Gabaldi <edgabaldi@gmail.com>
2013-01-10 18:12:31 -0200

    exclude user fields of ModelForms

--- money/forms.py
+++ money/forms.py
@@ -3,16 +3,20 @@ from money import models
 
 class EntryForm(forms.ModelForm):
     class Meta:
+        exclude = ('user',)
         model = models.Entry
 
 class BankForm(forms.ModelForm):
     class Meta:
+        exclude = ('user',)
         model = models.Bank
 
 class AccountForm(forms.ModelForm):
     class Meta:
+        exclude = ('user',)
         model = models.Account
 
 class PersonForm(forms.ModelForm):
     class Meta:
+        exclude = ('user',)
         model = models.Person
