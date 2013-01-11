2f4885cd40729292609b51c330f591959dde4689
Edgar gabaldi <edgabaldi@gmail.com>
2013-01-11 09:24:25 -0200

    Fix Identation in ModelFormWithListView class

--- money/generic.py
+++ money/generic.py
@@ -45,26 +45,26 @@ class LoginRequired(generic.View):
 
 class ModelFormWithListView(RestrictedListView, generic.edit.ModelFormMixin, generic.edit.ProcessFormView):
 
-	def post(self, request, *args, **kwargs):
-		try:
-			self.object = self.get_object()
-		except:
-			self.object = None
-		return super(ModelFormWithListView, self).post(request, *args, **kwargs)
+    def post(self, request, *args, **kwargs):
+        try:
+            self.object = self.get_object()
+        except:
+            self.object = None
+        return super(ModelFormWithListView, self).post(request, *args, **kwargs)
 
-	def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs):
 
-		self.object_list = self.get_queryset()
+        self.object_list = self.get_queryset()
 
-		try:
-			self.object = self.get_object()
-		except:
-			self.object = None
+        try:
+            self.object = self.get_object()
+        except:
+            self.object = None
 
-		kwargs.update({
-			'object_list' : self.object_list,
-			'form' : self.get_form(self.get_form_class()),
-			'object' : self.object
-		})
+        kwargs.update({
+            'object_list' : self.object_list,
+            'form' : self.get_form(self.get_form_class()),
+            'object' : self.object
+        })
 
-		return super(ModelFormWithListView, self).get_context_data(**kwargs)
+        return super(ModelFormWithListView, self).get_context_data(**kwargs)
